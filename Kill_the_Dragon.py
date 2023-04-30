aac13af84e1429809b61e665a98c3634f7212cd4
Andres Emiliano Moreno Rosales <Andres.Emiliano.mr@gmail.com>
2023-04-30 03:18:49 -0600

    another change to the language and adding armor and weapons

--- Kill_the_Dragon.py
+++ Kill_the_Dragon.py
@@ -1,10 +1,15 @@
 import random
 print("you have enter a dungeon and spotted a dragon far away")
-correr_o_quedarse = int(input("--will you run or stay 'press 1'to run 'press 2' to stay = "))
-if correr_o_quedarse == 1:
-    vives_o_mueres = random.randint(0,1)
-    if vives_o_mueres == 1:
+run_or_stay = int(input("--will you run or stay 'press 1'to run 'press 2' to stay = "))
+if run_or_stay == 1:
+    live_or_die = random.randint(0,1)
+    if live_or_die == 1:
         print('you will live, this is not the expected ending💖')
-    elif vives_o_mueres == 0:
+    elif live_or_die == 0:
         print('the dragon have burned your,Game Over💔')
+if run_or_stay == 2:
+    print("the game starts")
+    armor = ("dimon", "gold", "silver")
+    weapons = ("bow", "sword", "magic staff")
+    print ("you have found a set of armors and wapons that are compose by" , armor , 'and' , weapons)
 
